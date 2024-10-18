fa2fdad9393ac61b5f386f8bc10b832ea0b18e05
Nishat Rahman <168478182+Nishat-Rahman-web@users.noreply.github.com>
2024-10-18 09:03:44 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding:{
+        DEFAULT: '35px',
+      } ,
+    },
+    fontFamily: {
+      'montserrat': ["Montserrat", "sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
